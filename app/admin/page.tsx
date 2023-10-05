14c24e0abf0d31bed79c17b20c8dc272cb73ea80
mtoan12 <20120215@student.hcmus.edu.vn>
2023-10-05 23:44:58 +0700

    Fix category admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,3 @@
-import AdminCategory from './AdminCategory';
-import AdminProductsList from './AdminProductsList';
-
 export default function Admin() {
-    return (
-        <main className="pt-[48px] pb-[200px]">
-            <AdminCategory />
-        </main>
-    );
+    return <main className="pt-[48px] pb-[200px]"></main>;
 }
