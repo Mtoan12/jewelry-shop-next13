b25b13e38663db2907812280397bf4f03106e70b
mtoan12 <20120215@student.hcmus.edu.vn>
2023-11-18 12:50:38 +0700

    xóa admin khỏi project

--- app/admin/(main)/materials/page.tsx
+++
@@ -1,5 +0,0 @@
-import AdminCategory from "../../AdminCategory";
-
-export default function Admin() {
-    return <main className="py-[48px]"><AdminCategory /></main>;
-}
--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminCategory from './AdminCategory';
-
-export default function Admin() {
-    return (
-        <main className="pt-[48px] pb-[200px]">
-            <AdminCategory />
-        </main>
-    );
-}
