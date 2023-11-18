2e02381923bfa0de6c8931d1c878774c8af0a1b5
mtoan12 <20120215@student.hcmus.edu.vn>
2023-11-18 14:41:51 +0700

    redeploy

--- app/nhan-nam/page.tsx
+++ app/nhan-nam/page.tsx
@@ -5,7 +5,7 @@ import getProductsByCategory from '../api/getProductsByCategory';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Nhẫn nam',
+    title: 'Nhẫn nam - Tiệm vàng Kim Cúc',
     description:
         'Nhẫn Nam - Biểu Tượng Phong Cách và Đẳng Cấp.\r\n\r\nKhám phá bộ sưu tập nhẫn nam tại tiệm vàng Kim Cúc, nơi sự tinh tế và sang trọng hòa quyện. Những chiếc nhẫn được chế tác từ kim loại quý như vàng và bạc, đem đến sự lựa chọn hoàn hảo cho phong cách cá nhân của bạn.\r\n\r\nVới thiết kế hiện đại và đường nét tinh xảo, những chiếc nhẫn nam của chúng tôi không chỉ là trang sức, mà còn là biểu tượng của sự đẳng cấp. Từ nhẫn đơn giản đến mẫu đính đá tinh tế, mỗi chiếc nhẫn là một câu chuyện về phong cách riêng của người đeo. Khám phá ngay để tìm kiếm sự hoàn hảo cho phong cách nam tính và độc đáo của bạn.',
 };
